ff81a1a58b221f2d4cbbb984c8f98745411fb943
ChulHyun, Baek <chbaek@outlook.com>
2018-03-07 21:24:28 +0900

    Create UpdateAsg.py

---
+++ UpdateAsg.py
@@ -0,0 +1,56 @@
+# -- By chb@mz.co.kr -- # 
+# -- On Mar 6, 2018 -- # 
+import boto3 
+
+# -- This is a Session for AWS Connection -- # 
+def GetClient(Service):
+    session = boto3.Session()
+    client = session.client(Service)
+
+    return client
+
+def UpdateAutoScalingGroup(**Params):
+    client = GetClient("autoscaling")
+    response = None 
+    Min = Params['Min']
+    Desire = Params['Desire']
+    Max = Params['Max']
+    AutoScalingName = Params['Name']
+
+    try:
+        response = client.update_auto_scaling_group(
+            AutoScalingGroupName = AutoScalingName,
+            MinSize = Min,
+            MaxSize = Max,
+            DesiredCapacity = Desire,
+        )
+    except Exception as e:
+        print(e)
+    print(response)
+
+# -- def lambda_handler(event, context):
+if __name__=="__main__":
+
+    Params = [
+        {
+            'Name' : 'Prd-Asg',
+            'Max': 7,
+            'Min': 4,
+            'Desire': 4
+        },
+        {
+            'Name' : 'Dev-Asg',
+            'Max': 7,
+            'Min': 4,
+            'Desire': 4
+        },
+        {
+            'Name' : 'Stg-Asg',
+            'Max': 7,
+            'Min': 4,
+            'Desire': 4
+        }
+    ]
+
+    for i in Params:
+        UpdateAutoScalingGroup(**i)
